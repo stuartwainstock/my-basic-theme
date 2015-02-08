15e5dffc45790cc1e39a6ab11862416a012154e8
Stuart Wainstock <stuart.wainstock@gmail.com>
2015-02-08 14:27:47 -0600

    Remove static title replace with bloginfo WP call

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <html lang="en">
 <head>
-	<title>My Super Simple WordPress Theme</title>
+	<title><?php bloginfo(); ?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" type="text/css" href="style.css" >
 </head>
