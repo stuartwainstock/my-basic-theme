ae75ae69332c8732ad7de916346b2d25db8fdde4
Stuart Wainstock <stuart.wainstock@gmail.com>
2015-02-08 14:21:21 -0600

    wk3 my-basic-theme initial commit

---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+	<title>My Super Simple WordPress Theme</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css" >
+</head>
+
+<body>
+
+	<header>
+		<h1>Stuart Wainstock | Designer</h1>
+		<nav>
+			<ul>
+				<li><a href="">Home</a></li>
+				<li><a href="">About Us</a></li>
+				<li><a href="">Services</a></li>
+				<li><a href="">Portfolio</a></li>
+				<li><a href="">Contact Us</a></li>
+			</ul>
+		</nav> 
+    </header>
+
+    <section>
+    	<div>
+			<article>
+				<h1><a href="">This is an Article Title</a></h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla nisi, adipiscing eu laoreet vitae, venenatis vitae velit. Phasellus euismod dapibus velit in laoreet. Vivamus ornare justo vehicula felis scelerisque non aliquam nisl semper. Curabitur nisl mauris, posuere sed imperdiet vel, cursus id dolor. Suspendisse varius consequat lorem ac luctus. Maecenas consectetur neque at turpis elementum vitae eleifend sem blandit. Nullam auctor, risus nec porta lacinia, ante sapien bibendum massa, a semper tortor odio in nunc. </p>
+			</article>
+			<article>
+				<h1><a href="">This is an Article Title</a></h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla nisi, adipiscing eu laoreet vitae, venenatis vitae velit. Phasellus euismod dapibus velit in laoreet. Vivamus ornare justo vehicula felis scelerisque non aliquam nisl semper. Curabitur nisl mauris, posuere sed imperdiet vel, cursus id dolor. Suspendisse varius consequat lorem ac luctus. Maecenas consectetur neque at turpis elementum vitae eleifend sem blandit. Nullam auctor, risus nec porta lacinia, ante sapien bibendum massa, a semper tortor odio in nunc. </p>
+			</article>
+			<article>
+				<h1><a href="">This is an Article Title</a></h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla nisi, adipiscing eu laoreet vitae, venenatis vitae velit. Phasellus euismod dapibus velit in laoreet. Vivamus ornare justo vehicula felis scelerisque non aliquam nisl semper. Curabitur nisl mauris, posuere sed imperdiet vel, cursus id dolor. Suspendisse varius consequat lorem ac luctus. Maecenas consectetur neque at turpis elementum vitae eleifend sem blandit. Nullam auctor, risus nec porta lacinia, ante sapien bibendum massa, a semper tortor odio in nunc. </p>
+			</article>
+			<article>
+				<h1><a href="">This is an Article Title</a></h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla nisi, adipiscing eu laoreet vitae, venenatis vitae velit. Phasellus euismod dapibus velit in laoreet. Vivamus ornare justo vehicula felis scelerisque non aliquam nisl semper. Curabitur nisl mauris, posuere sed imperdiet vel, cursus id dolor. Suspendisse varius consequat lorem ac luctus. Maecenas consectetur neque at turpis elementum vitae eleifend sem blandit. Nullam auctor, risus nec porta lacinia, ante sapien bibendum massa, a semper tortor odio in nunc. </p>
+			</article>
+			<article>
+				<h1><a href="">This is an Article Title</a></h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla nisi, adipiscing eu laoreet vitae, venenatis vitae velit. Phasellus euismod dapibus velit in laoreet. Vivamus ornare justo vehicula felis scelerisque non aliquam nisl semper. Curabitur nisl mauris, posuere sed imperdiet vel, cursus id dolor. Suspendisse varius consequat lorem ac luctus. Maecenas consectetur neque at turpis elementum vitae eleifend sem blandit. Nullam auctor, risus nec porta lacinia, ante sapien bibendum massa, a semper tortor odio in nunc. </p>
+			</article>
+			<article>
+				<h1><a href="">This is an Article Title</a></h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla nisi, adipiscing eu laoreet vitae, venenatis vitae velit. Phasellus euismod dapibus velit in laoreet. Vivamus ornare justo vehicula felis scelerisque non aliquam nisl semper. Curabitur nisl mauris, posuere sed imperdiet vel, cursus id dolor. Suspendisse varius consequat lorem ac luctus. Maecenas consectetur neque at turpis elementum vitae eleifend sem blandit. Nullam auctor, risus nec porta lacinia, ante sapien bibendum massa, a semper tortor odio in nunc. </p>
+			</article>
+			<article>
+				<h1><a href="">This is an Article Title</a></h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla nisi, adipiscing eu laoreet vitae, venenatis vitae velit. Phasellus euismod dapibus velit in laoreet. Vivamus ornare justo vehicula felis scelerisque non aliquam nisl semper. Curabitur nisl mauris, posuere sed imperdiet vel, cursus id dolor. Suspendisse varius consequat lorem ac luctus. Maecenas consectetur neque at turpis elementum vitae eleifend sem blandit. Nullam auctor, risus nec porta lacinia, ante sapien bibendum massa, a semper tortor odio in nunc. </p>
+			</article>
+			<article>
+				<h1><a href="">This is an Article Title</a></h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla nisi, adipiscing eu laoreet vitae, venenatis vitae velit. Phasellus euismod dapibus velit in laoreet. Vivamus ornare justo vehicula felis scelerisque non aliquam nisl semper. Curabitur nisl mauris, posuere sed imperdiet vel, cursus id dolor. Suspendisse varius consequat lorem ac luctus. Maecenas consectetur neque at turpis elementum vitae eleifend sem blandit. Nullam auctor, risus nec porta lacinia, ante sapien bibendum massa, a semper tortor odio in nunc. </p>
+			</article>
+			<article>
+				<h1><a href="">This is an Article Title</a></h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla nisi, adipiscing eu laoreet vitae, venenatis vitae velit. Phasellus euismod dapibus velit in laoreet. Vivamus ornare justo vehicula felis scelerisque non aliquam nisl semper. Curabitur nisl mauris, posuere sed imperdiet vel, cursus id dolor. Suspendisse varius consequat lorem ac luctus. Maecenas consectetur neque at turpis elementum vitae eleifend sem blandit. Nullam auctor, risus nec porta lacinia, ante sapien bibendum massa, a semper tortor odio in nunc. </p>
+			</article>
+			<article>
+				<h1><a href="">This is an Article Title</a></h1>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nulla nisi, adipiscing eu laoreet vitae, venenatis vitae velit. Phasellus euismod dapibus velit in laoreet. Vivamus ornare justo vehicula felis scelerisque non aliquam nisl semper. Curabitur nisl mauris, posuere sed imperdiet vel, cursus id dolor. Suspendisse varius consequat lorem ac luctus. Maecenas consectetur neque at turpis elementum vitae eleifend sem blandit. Nullam auctor, risus nec porta lacinia, ante sapien bibendum massa, a semper tortor odio in nunc. </p>
+			</article>
+		</div>
+</body>
+</html>
